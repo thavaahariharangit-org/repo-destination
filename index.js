5d2d2007f16d61e44e85bbff95ae1d7292750ca3
Hariharan Thavachelvam <164553783+thavaahariharangit@users.noreply.github.com>
2024-12-23 14:42:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+// index.js
+const core = require('@actions/core');
 
+async function run() {
+  try {
+    const exampleInput = core.getInput('example-input');
+    console.log(`Example input: ${exampleInput}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
